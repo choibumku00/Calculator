7b8593149be2b81afd73232bb972e1a818f17f6b
choibumku00 <101037541+choibumku00@users.noreply.github.com>
2022-05-04 09:30:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
   author='Bumku.Choi',
   author_email='choirony00@gmail.com',
   url='https://github.com/choibumku00/Calculator',
+  download_url='https://github.com/choibumku00/Calculator/archive/refs/tags/v0.1.0.zip'
   install_requires=['pytest'],
   long_description='my calculator python module',
   long_description_content_type='text/markdown',
