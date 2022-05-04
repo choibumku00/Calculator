2aa5f2ca6937618d8604694b369c6ed6f08da6f3
choibumku00 <101037541+choibumku00@users.noreply.github.com>
2022-05-04 09:26:59 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = Ture,
+  name='mycalculator2929',
+  version='1.0.0',
+  description='my calculator2929',
+  author='Bumku.Choi',
+  author_email='choirony00@gmail.com',
+  url='https://github.com/choibumku00/Calculator',
+  install_requires=['pytest'],
+  long_description='my calculator python module',
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
